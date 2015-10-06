3b27984b3417a6fa077c5c2aa0971fe0e79c961b
nlambuca <nunolambuca@gmail.com>
2015-10-05 22:23:29 -0700

    Update MAX78615_SPI.h

--- firmware/MAX78615_SPI.h
+++ firmware/MAX78615_SPI.h
@@ -6,10 +6,10 @@ class MAX78615
    public:
       MAX78615_LMU(uint8_t);      
       
-      void write_registers(utin8_t addr, uint8_t to_write, uint32_t words[]);
-      void write_register(uint8_t addr, uint32_t word);
-      void read_registers(uint8_t addr, uint8_t to_read, uint32_t words[]);
-      uint32_t read_register(uint8_t addr);
+      void write_registers(utin8_t, uint8_t , uint32_t words[]);
+      void write_register(uint8_t, uint32_t );
+      void read_registers(uint8_t, uint8_t to_read, uint32_t words[]);
+      uint32_t read_register(uint8_t);
       
       //-MSBFIRST
       //-1MHZ max
@@ -17,6 +17,9 @@ class MAX78615
    private:
       uint8_t  SS;
       void init(uint8_t ss);
+      void send_control_byte(uint8_t, uint8_t, uint8_t );
+      void writebyte(uint8_t);
+      uint8_t readbyte(uint8_t);
 };
 #endif
 
